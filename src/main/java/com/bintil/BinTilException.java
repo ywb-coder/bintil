aefdcf9541dfe2dd6f834e348625af7e022ec5c8
ywb <1364583806@qq.com>
2023-08-10 19:46:04 +0800

    commit

--- src/main/java/com/bintil/BinTilException.java
+++ src/main/java/com/bintil/BinTilException.java
@@ -2,11 +2,9 @@ package com.bintil;
 
 /**
  * @Author ywb
- * @Package com.bintil
  * @Date 2023/8/10 18:59
  */
 public class BinTilException extends RuntimeException {
-
     public BinTilException(String message) {
         super(message);
     }
